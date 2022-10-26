c6791d41c8f85c7f3eba6392a2e36c84c263e565
JarmoTro <82982361+JarmoTro@users.noreply.github.com>
2022-10-26 21:02:12 +0300

    Add user page

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import landingPage from '../views/landingPage.vue'
 import authPage from '../views/authPage.vue'
 import welcomePage from '../views/welcomePage.vue'
+import userPage from '../views/userPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
       path: '/welcome',
       name: 'welcome',
       component: welcomePage
+    },
+    {
+      path: '/myTasks',
+      name: 'userPage',
+      component: userPage
     },
   ]
 })
