626e02c962fddafb99c08cd50f62b8f760663f02
JarmoTro <82982361+JarmoTro@users.noreply.github.com>
2022-10-26 20:02:14 +0300

    Add auth page

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import landingPage from '../views/landingPage.vue'
+import authPage from '../views/authPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,17 @@ const router = createRouter({
       path: '/',
       name: 'LandingPage',
       component: landingPage
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: authPage
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: authPage
+    },
   ]
 })
 
